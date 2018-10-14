c342948d8346f635571899b916f4da966b3b0ddf
Christopher Loessl <cloessl+github@gmail.com>
2018-10-14 12:32:38 +0200

    Add: Basic vue-hoodie construct

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueHoodie from 'vue-hoodie';
+Vue.use(VueHoodie);
+
+import Hoodie from '@hoodie/client';
+import PouchDB from 'pouchdb';
+
+const hoodie = new Hoodie({
+  PouchDB,
+  url: 'http://localhost:8084'
+});
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  hoodie: hoodie
 }).$mount('#app')
