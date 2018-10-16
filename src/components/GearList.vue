476b5453436b31d2a50ad23915df94cf4806a458
Stefan Strigler <stefan.strigler@1und1.de>
2018-10-16 19:42:47 +0200

    toggle editable

--- src/components/GearList.vue
+++ src/components/GearList.vue
@@ -1,7 +1,14 @@
 <template>
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2"><input type="text" class="form-control" v-model.trim="list.name" @input="updateList"/></h1>
+    <h1 class="h2">
+      <span @click="toggleEditable" v-show="!editable">{{list.name}}</span>
+      <input type="text"
+             class="form-control"
+             v-show="editable"
+             v-model.trim="list.name"
+             @input="updateList"
+             @blur="toggleEditable"/></h1>
     <div class="btn-toolbar mb-2 mb-md-0">
       <div class="btn-group mr-2">
         <button class="btn btn-sm btn-outline-secondary">Share</button>
@@ -28,6 +35,11 @@ export default {
   components: {
     GearListCategory
   },
+  data() {
+    return {
+      editable: false
+    }
+  },
   props: {
     list: {
       type: Object,
@@ -35,6 +47,9 @@ export default {
     }
   },
   methods: {
+    toggleEditable() {
+      this.editable=!this.editable;
+    },
     updateList() {
         this.gearListStore.updateOrAdd(this.list);
     }
