883e3f585f7b56afa4579fec494b809a9b2610a3
Stefan Strigler <stefan.strigler@1und1.de>
2018-10-16 21:41:02 +0200

    fix jumping to new list

--- src/App.vue
+++ src/App.vue
@@ -86,7 +86,7 @@ export default {
       this.gearListStore.add(newList).then(
         list => {
           this.lists.push(list);
-          this.setCurrentList(list);
+          this.setCurrentList(list._id);
         }
       )
     },
