504dfe98215311c7ef84b62c700439f5ada8bd50
Stefan Strigler <stefan@strigler.de>
2018-10-29 20:09:09 +0100

    simplify

--- src/components/GearList.vue
+++ src/components/GearList.vue
@@ -21,41 +21,46 @@
     </div>
   </div>
 
-  <div class="table-responsive">
-    <table class="table table-striped table-sm">
-      <tbody>
-        <tr
-          v-for="(category, idx) in list.categories"
-          :key="idx"
-          >
-          <td>{{category.name}}</td>
-          <td>{{sumWeights(category.items)}}</td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <th>Total</th>
-          <td>{{totalWeight()}}</td>
-        </tr>
-        <tr>
-          <th>Base Weight</th>
-          <td>{{baseWeight()}}</td>
-        </tr>
-        <tr>
-          <th>Worn</th>
-          <td>{{wornWeight()}}</td>
-        </tr>
-        <tr>
-          <th>Consumables</th>
-          <td>{{consumablesWeight()}}</td>
-        </tr>
-        <tr>
-          <th>Pack</th>
-          <td>{{packWeight()}}</td>
-        </tr>
-      </tfoot>
-    </table>
-  </div>
+  <table class="table table-respsonsive table-sm">
+    <thead class="thead-dark">
+      <tr>
+        <th>Category</th>
+        <th>Weight</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(category, idx) in list.categories"
+        :key="idx"
+        >
+        <td>{{category.name}}</td>
+        <td>{{categoryWeight(category, () => true)}}</td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <th>Total</th>
+        <td>{{totalWeight(() => true)}}</td>
+      </tr>
+      <tr>
+        <th>Base Weight</th>
+        <td>{{totalWeight((item) => item.category != 'consumable' && item.category != 'worn')}}</td>
+      </tr>
+      <tr>
+        <th>Worn</th>
+        <td>{{totalWeight((item) => item.category == 'worn')}}</td>
+      </tr>
+      <tr>
+        <th>Consumables</th>
+        <td>{{totalWeight((item) => item.category == 'consumable')}}</td>
+      </tr>
+      <tr>
+        <th>Pack</th>
+        <td>{{totalWeight((item) => item.category != 'worn' && item.category != 'consumable')}}</td>
+      </tr>
+    </tfoot>
+  </table>
+  
   <!--canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas-->
   <GearListCategory
     v-for="(category, idx) in list.categories"
@@ -101,28 +106,11 @@ export default {
     updateList() {
       this.gearListStore.updateOrAdd(this.list);
     },
-    sumWeights(items, filter) {
-      if (!filter) filter = () => true;
-      return items.filter(filter).reduce((sum, item) => sum + item.weight, 0);
+    categoryWeight(category, filter) {
+      return category.items.filter(filter).reduce((sum, item) => sum + item.weight, 0);
     },
-    totalWeight() {
-      return this.list.categories.reduce((sum, category) => sum + this.sumWeights(category.items), 0);
-    },
-    baseWeight() {
-      const filter = (item) => item.category != 'worn' && item.category != 'consumable';
-      return this.list.categories.reduce((sum, category) => sum + this.sumWeights(category.items, filter), 0);
-    },
-    consumablesWeight() {
-      const filter = (item) => item.category == 'consumable';
-      return this.list.categories.reduce((sum, category) => sum + this.sumWeights(category.items, filter), 0);
-    },
-    packWeight() {
-      const filter = (item) => item.category != 'worn';
-      return this.list.categories.reduce((sum, category) => sum + this.sumWeights(category.items, filter), 0);
-    },
-    wornWeight() {
-      const filter = (item) => item.category == 'worn';
-      return this.list.categories.reduce((sum, category) => sum + this.sumWeights(category.items, filter), 0);
+    totalWeight(filter) {
+      return this.list.categories.reduce((sum, category) => sum + this.categoryWeight(category, filter), 0);
     }
   },
   created() {
