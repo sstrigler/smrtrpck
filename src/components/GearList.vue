070976d56dcabbe12035e1e75cf41f1d05f7c3ed
Stefan Strigler <stefan@strigler.de>
2018-10-30 09:52:47 +0100

    remove stale canvas

--- src/components/GearList.vue
+++ src/components/GearList.vue
@@ -61,7 +61,6 @@
     </tfoot>
   </table>
   
-  <!--canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas-->
   <GearListCategory
     v-for="(category, idx) in list.categories"
     :key="idx"
