0682ffcfe541de82323f02aa7e1041f3ea22a664
Stefan Strigler <stefan@strigler.de>
2018-11-08 19:01:41 +0100

    fix format

--- src/components/GearList.vue
+++ src/components/GearList.vue
@@ -53,10 +53,8 @@
       </tr>
     </thead>
     <tbody>
-      <tr
-        v-for="(category, idx) in categories"
-        :key="idx"
-        >
+      <tr v-for="(category, idx) in categories"
+          :key="idx">
         <td><a :href="'#cat-' + list._id + idx">{{category.name}}</a></td>
         <td class="text-right font-weight-bold">{{convertToTotalsUnit(categoryWeight(category, () => true))}}</td>
         <td class="font-weight-bold ">{{list.totalsUnit}}</td>
