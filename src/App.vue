844d5b373e06dfe57cd181f033c413566dc68200
Stefan Strigler <stefan@strigler.de>
2018-11-08 14:59:35 +0100

    make add list button too

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,14 @@
     <nav class="col-md-2 d-none d-md-block bg-light sidebar">
       <div class="sidebar-sticky">
 
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span>Lists</span>
-          <a class="d-flex align-items-center text-muted"
-             href="#"
-             @click.prevent="addNewList"
-             title="Add new list">
+        <h6 class="sidebar-heading d-flex justify-content-start align-items-center px-3 mt-4 mb-1 text-muted">
+          Lists
+          &nbsp;
+          <button class="feather-button"
+                  title="Add new list"
+                  @click="addNewList">
             <plus-circle-icon class="feather"></plus-circle-icon>
-          </a>
+          </button>
         </h6>
         <ul class="nav flex-column mb-2">
           <li
